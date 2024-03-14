a33b6fd52ce7af223134593c76f7d8ca8cadee58
Chiragshiyani4490 <dhavalleelawala@gmail.com>
2024-03-14 10:22:20 +0530

    update

---
+++ 01_Basics/js/introduction.js
@@ -0,0 +1,22 @@
+// console.log("Hello js user..");
+
+// var fullName = "Dhaval P. Leelawala";
+
+// // console.log(fullName);
+
+// console.log("Full Name is: "+fullName);
+
+// var a = 10;
+// var b = 20;
+// console.log("Number is:"+num);
+
+// console.log("sum of a & b is:", a+b);
+
+// var a=4,b=4,sum;
+
+// console.log("a is:",a);
+// console.log("b is:",b);
+// // console.log("Sum is:",a+b);
+// sum=a+b;
+// console.log("sum is:",sum);
+
---
+++ addToCart/js/addProduct.js
@@ -0,0 +1,33 @@
+let product = [];
+
+let title = document.querySelector("#title");
+let category = document.querySelector("#category");
+let price = document.querySelector("#price");
+let oldprice = document.querySelector("#oldprice");
+let color = document.querySelector("#color");
+let description = document.querySelector("#description");
+let img = document.querySelector("#imgfile");
+
+function addProduct(){
+  
+  let obj = {
+    title : title.value,
+    category : category.value,
+    price : price.value,
+    oldprice : oldprice.value,
+    color : color.value,
+    description : description.value,
+    img : img.files[0].name
+  }
+  
+  product.push(obj);
+  localStorage.setItem('product',JSON.stringify(product));
+  storData();
+}
+
+function storData(){
+  let dataBase = JSON.parse(localStorage.getItem("product"));
+  product = dataBase;
+}
+
+
